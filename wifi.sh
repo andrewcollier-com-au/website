ae367f3f5a0a47358974ea5f2834b82df767c909
AndyRooAU <andrew@andrewcollier.com.au>
2024-06-15 08:36:21 +0800

    Create wifi.sh
    wifi setup script

---
+++ wifi.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "deb http://deb.debian.org/debian bookworm main contrib non-free-firmware non-free"
+
+apt-get update
+apt-get install linux-image-$(uname -r|sed 's,[^-]*-[^-]*-,,') linux-headers-$(uname -r|sed 's,[^-]*-[^-]*-,,') broadcom-sta-dkms
+modprobe -r b44 b43 b43legacy ssb brcmsmac bcma
+modprobe wl
