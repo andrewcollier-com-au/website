8e6010992b45e3e391a99d03922c6e3e52c86f8f
AndyRooAU <andrew@andrewcollier.com.au>
2024-06-17 06:47:46 +0800

    updating depencies
    adding sudo

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "First Install Dependencies"
 apt update
-apt install htop wget refind 
+apt install htop wget refind sudo
 
 wget https://raw.githubusercontent.com/andrewcollier-com-au/website/main/update-sources.sh
 chmod +x update-sources.sh
--- wifi.sh
+++ wifi.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Installing Wifi Module"
+echo "need to remove the old kernal, it gets confused"
 
 apt update
 apt install linux-image-$(uname -r|sed 's,[^-]*-[^-]*-,,') linux-headers-$(uname -r|sed 's,[^-]*-[^-]*-,,') broadcom-sta-dkms
