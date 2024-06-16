8f4707d076083dfa5ef29b67e8a478c0b9972844
AndyRooAU <andrew@andrewcollier.com.au>
2024-06-16 19:15:47 +0800

    Update update-sources.sh
    removing sudo, as we are clean install at this point and sudo is not installed

--- update-sources.sh
+++ update-sources.sh
@@ -20,15 +20,15 @@ mv /etc/apt/sources.list /etc/apt/sources.list.backup
 touch /etc/apt/sources.list
 
 # Add 'main contrib non-free-firmware non-free' to new repository file
-    echo "deb ${REPO_URL} ${DISTRIBUTION} main contrib non-free-firmware non-free" | sudo tee -a /etc/apt/sources.list
+    echo "deb ${REPO_URL} ${DISTRIBUTION} main contrib non-free-firmware non-free" | tee -a /etc/apt/sources.list
 
 # Ensure update and security repositories are also included
 if ! grep -q "^deb ${REPO_URL} ${DISTRIBUTION}-updates main" /etc/apt/sources.list; then
-    echo "deb ${REPO_URL} ${DISTRIBUTION}-updates main contrib non-free-firmware non-free" | sudo tee -a /etc/apt/sources.list
+    echo "deb ${REPO_URL} ${DISTRIBUTION}-updates main contrib non-free-firmware non-free" | tee -a /etc/apt/sources.list
 fi
 
 if ! grep -q "^deb ${REPO_URL} ${DISTRIBUTION}-security main" /etc/apt/sources.list; then
-    echo "deb ${REPO_URL} ${DISTRIBUTION}-security main contrib non-free-firmware non-free" | sudo tee -a /etc/apt/sources.list
+    echo "deb ${REPO_URL} ${DISTRIBUTION}-security main contrib non-free-firmware non-free" | tee -a /etc/apt/sources.list
 fi
 
 # Update package lists
